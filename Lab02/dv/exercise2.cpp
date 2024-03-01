49fcc8706049b92764328070db06b880597dcd64
Geoffrey Guindine <geoffreyguindine@geoffreys-air.mynetworksettings.com>
2024-03-01 00:19:35 -0500

    Lab 3 done

--- Lab02/dv/exercise2.cpp
+++ Lab02/dv/exercise2.cpp
@@ -2,35 +2,40 @@
 #include <cstdint>
 
 #include <catch2/catch_test_macros.hpp>
-// #include <VExercise2.h>
+#include <VExercise2.h>
 
-struct LFSR {
+struct LFSR
+{
   uint16_t value;
-  static constexpr uint16_t mask {0xB400};
+  static constexpr uint16_t mask{0xB400};
 
-  void step() {
+  void step()
+  {
     uint16_t bits = value & mask;
     value = (value << 1) | (std::popcount(bits) & 1);
   }
 };
 
-void step(VExercise2& model) {
+void step(VExercise2 &model)
+{
   model.clk = 0;
   model.eval();
   model.clk = 1;
   model.eval();
 }
 
-void test_initial_value(uint16_t init) {
+void test_initial_value(uint16_t init)
+{
   VExercise2 model;
   model.init = init;
   model.nReset = 0;
   step(model);
   model.nReset = 1;
 
-  LFSR lfsr {init};
+  LFSR lfsr{init};
 
-  for(uint8_t cycles {0}; cycles < 100; ++cycles) {
+  for (uint8_t cycles{0}; cycles < 100; ++cycles)
+  {
     REQUIRE(model.out == lfsr.value);
     step(model);
     lfsr.step();
@@ -41,18 +46,22 @@ void test_initial_value(uint16_t init) {
   REQUIRE(model.out == init);
 }
 
-TEST_CASE("Initial value 0x00") {
+TEST_CASE("Initial value 0x00")
+{
   test_initial_value(0x00);
 }
 
-TEST_CASE("Initial value 0xFF") {
+TEST_CASE("Initial value 0xFF")
+{
   test_initial_value(0xFF);
 }
 
-TEST_CASE("Initial value 0x55") {
+TEST_CASE("Initial value 0x55")
+{
   test_initial_value(0x55);
 }
 
-TEST_CASE("Initial value 0xAA") {
+TEST_CASE("Initial value 0xAA")
+{
   test_initial_value(0xAA);
 }
